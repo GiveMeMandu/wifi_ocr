1b7f8601b82d8d28c1072b14f727aa2272bb092a
김우찬 <52920226+GiveMeMandu@users.noreply.github.com>
2022-02-28 23:34:34 +0900

    add scanPage comments

--- lib/sub/scanPage.dart
+++ lib/sub/scanPage.dart
@@ -16,11 +16,13 @@ class ScanPage extends StatelessWidget {
                   ElevatedButton(
                     child: Text('Scan Text/QR'),
                     onPressed: () {
+                      //카메라 페이지로 이동
                     },
                   ),
                   ElevatedButton(
                     child: Text('Edit'),
                     onPressed: () {
+                      //편집가능한 텍스트 필드 있는 페이지로 이동
                     },
                   ),
                 ],
@@ -31,6 +33,7 @@ class ScanPage extends StatelessWidget {
               ElevatedButton(
                 child: Text('Connect & Save'),
                 onPressed: () {
+                  //wifi 연결 시도 후 성공 시 wifi클래스 정보 담은 QR생성 및 List에 추가
                 },
               ),
             ],
