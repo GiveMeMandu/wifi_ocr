3b03df80653d170d60bf268f4e48d7b3910c2753
김우찬 <52920226+GiveMeMandu@users.noreply.github.com>
2022-03-01 02:19:07 +0900

    card -> listTile

--- lib/sub/wifiListPage.dart
+++ lib/sub/wifiListPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../wifiItem.dart';
 
-
 class WifiListPage extends StatefulWidget {
   final List<Wifi> list; // wifi List 선언
   WifiListPage({Key? key, required this.list}) : super(key: key);
@@ -11,62 +10,62 @@ class WifiListPage extends StatefulWidget {
 }
 
 class _WifiListPage extends State<WifiListPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
           child: ReorderableListView.builder(
-              onReorder: (oldIndex,newIndex){
+              itemBuilder: (context, position) {
+                return ListTile(
+                  key: Key('$position'),
+                  leading: GestureDetector(
+                    child: Image.asset(
+                      widget.list[position].imagePath!,
+                      fit: BoxFit.contain,
+                    ),
+                    onTap: () async {
+                      //QR만 확대
+                      await showDialog(
+                          context: context,
+                          builder: (_) => qrDialog(
+                              widget.list[position].imagePath!, context));
+                    },
+                  ),
+                  title: GestureDetector(
+                    child: Row(children: <Widget>[
+                      Text(
+                        widget.list[position].name!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Column(children: <Widget>[
+                        Text(
+                          "SSID : " + widget.list[position].ssid!,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "Bands : 2.4Ghz", //getBandType()
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ]),
+                    ]),
+                    onTap: () async {
+                      //세부정보 표시
+                      await showDialog(
+                          context: context,
+                          builder: (_) =>
+                              detailDialog(widget.list[position], context));
+                    },
+                  ),
+                  trailing: Icon(Icons.more_vert),
+                );
+              },
+              onReorder: (oldIndex, newIndex) {
                 setState(() {
                   _updateItems(oldIndex, newIndex);
                 });
-              },
-              itemBuilder: (context, position) {
-                return GestureDetector(
-                  key: Key('$position'),
-                  child: Card(
-                    child: Row(
-                      children: <Widget>[
-                        GestureDetector(
-                          child: Image.asset(
-                            widget.list[position].imagePath!,
-                            height: 100,
-                            width: 100,
-                            fit: BoxFit.contain,
-                          ),
-                          onTap: () async {//QR만 확대
-                            await showDialog(
-                                context: context,
-                                builder: (_) => qrDialog(widget.list[position].imagePath!, context)
-                            );
-                          },
-                        ),
-                        Text(widget.list[position].name!,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-                        Column(
-                          children: <Widget>[
-                            Text("SSID : " + widget.list[position].ssid!,
-                            style: TextStyle(fontSize: 12),),
-                            Text("Bands : 2.4Ghz",//getBandType()
-                              style: TextStyle(fontSize: 12),),
-                          ]
-                        ),
-                      ],
-                    ),
-                  ),
-                  onTap: () async {//세부정보 표시
-                    await showDialog(
-                        context: context,
-                        builder: (_) => detailDialog(widget.list[position], context)
-                    );
-                  },
-                  onLongPress: () {
-                    //리스트뷰 내 카드 위치 편집
-                  },
-                );
               },
               itemCount: widget.list.length),
         ),
@@ -75,13 +74,12 @@ class _WifiListPage extends State<WifiListPage> {
   }
 
   void _updateItems(int oldIndex, int newIndex) {
-    if(newIndex > oldIndex){
+    if (newIndex > oldIndex) {
       newIndex -= 1;
     }
 
     final item = widget.list.removeAt(oldIndex);
     widget.list.insert(newIndex, item);
-
   }
 }
 
@@ -114,9 +112,10 @@ Widget qrDialog(path, context) {
         ),
       ],
     ),
-  );}
+  );
+}
 
-Widget detailDialog(wifi, context){
+Widget detailDialog(wifi, context) {
   return Dialog(
     // backgroundColor: Colors.transparent,
     // elevation: 0,
@@ -136,17 +135,17 @@ Widget detailDialog(wifi, context){
           ),
         ),
         Container(
-          width: 350,
-          height: 350,
-          child: Column(
-            children: <Widget>[
-              Text('Name: ${wifi.name}'),
-              Text('SSID: ${wifi.ssid}'),
-              Text('PW  : ${wifi.pw}'),
-              // 이후 추가되는 속성들 추가해주세요
-            ],
-          )
-        ),
+            width: 350,
+            height: 350,
+            child: Column(
+              children: <Widget>[
+                Text('Name: ${wifi.name}'),
+                Text('SSID: ${wifi.ssid}'),
+                Text('PW  : ${wifi.pw}'),
+                // 이후 추가되는 속성들 추가해주세요
+              ],
+            )),
       ],
     ),
-  );}
+  );
+}
